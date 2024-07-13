bcc0d40240b3c35151b3846712bdc1d19f466546
mariopuff <supermariomario184third@gmail.com>
2024-07-13 23:10:52 +0700

    mobile support and socials section
    HOLY CRAP THAT'S A LOT!!

---
+++ mobile-menu.js
@@ -0,0 +1,9 @@
+// menu.js
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', function() {
+        navLinks.classList.toggle('active');
+    });
+});
